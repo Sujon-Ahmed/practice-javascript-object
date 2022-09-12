71ab7aa13d6eb8c9d36e2d41c1d387124824b35f
Sujon-Ahmed <sujonahmed5284@gmail.com>
2022-09-12 12:46:35 +0600

    object with function

--- objects/object.js
+++ objects/object.js
@@ -5,14 +5,20 @@ const car = {
 }
 
 const person = {
-    name: 'sujon',
+    firstName: 'sujon',
+    lastName: 'ahmed',
     eye: 'black',
-    age: 22
+    age: 22,
+    fullName: function() {
+        return `${this.firstName} ${this.lastName}`;
+    }
 }
 
 // console.table(car);
 // console.log(car.type);
 
 // console.table(person);
-console.log(person.name); // dot notation
-console.log(person['age']); // array notation
+//console.log(person.name); // dot notation
+//console.log(person['age']); // array notation
+
+console.log(person.fullName());
